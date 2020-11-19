a1a65cfaf2a54e73821bc3405d0062bce6023728
Christian Hettlage <hettlage@saao.ac.za>
2020-11-19 11:56:18 +0200

    Set up tooling

---
+++ storage/__init__.py
@@ -0,0 +1,3 @@
+"""Storage of SALT proposal content."""
+
+__version__ = "0.1.0"
---
+++ tests/test_storage.py
@@ -0,0 +1,8 @@
+"""Test storage."""
+
+from storage import __version__
+
+
+def test_version():
+    """Dummy test."""
+    assert __version__ == "0.1.0"
